d7931251b2752d4ec5b023a228cbace1f8fcf51c
Ryan Santos <ryansantos86@gmail.com>
2019-05-14 16:25:47 -0700

    login field states

--- frontend/components/Signin/Signin.js
+++ frontend/components/Signin/Signin.js
@@ -1,32 +1,57 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FaEnvelope, FaKey } from 'react-icons/fa';
 
 import SigninStyles from './SigninStyles';
 import Form from '../shared/Form';
 
-const Signin = ({ setIsSignup }) => (
-  <SigninStyles>
-    <Form>
-      <fieldset>
-        <h2>Sign into your account</h2>
-        <div className="input-group">
-          <div className="input-container">
-            <FaEnvelope className="fa" />
-            <input type="email" name="email" placeholder="Email" />
+const Signin = ({ setIsSignup }) => {
+  const [fields, setFields] = useState({
+    email: '',
+    password: '',
+  });
+
+  const saveFields = e => {
+    setFields({ ...fields, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <SigninStyles>
+      <Form>
+        <fieldset>
+          <h2>Sign into your account</h2>
+          <div className="input-group">
+            <div className="input-container">
+              <FaEnvelope className="fa" />
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={fields.email}
+                onChange={saveFields}
+              />
+            </div>
+            <div className="input-container">
+              <FaKey className="fa" />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={fields.password}
+                onChange={saveFields}
+                autoComplete="true"
+              />
+            </div>
           </div>
-          <div className="input-container">
-            <FaKey className="fa" />
-            <input type="password" name="password" placeholder="Password" />
-          </div>
-        </div>
-        <button type="submit">Sign In</button>
-      </fieldset>
-    </Form>
-    <p>
-      Are you new? <a onClick={() => setIsSignup(true)}>Sign Up</a>
-    </p>
-  </SigninStyles>
-);
+          <button type="submit">Sign In</button>
+        </fieldset>
+      </Form>
+      <p>
+        Are you new? <a onClick={() => setIsSignup(true)}>Sign Up</a>
+      </p>
+    </SigninStyles>
+  );
+};
 
 Signin.propTypes = {
   setIsSignup: PropTypes.func.isRequired,
--- frontend/components/Signup/Signup.js
+++ frontend/components/Signup/Signup.js
@@ -1,36 +1,69 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FaEnvelope, FaUser, FaKey } from 'react-icons/fa';
 
 import SignupStyles from './SignupStyles';
 import Form from './../shared/Form';
 
-const Signup = ({ setIsSignup }) => (
-  <SignupStyles>
-    <Form>
-      <fieldset>
-        <h2>Sign up for an account</h2>
-        <div className="input-group">
-          <div className="input-container">
-            <FaEnvelope className="fa" />
-            <input type="email" name="email" placeholder="Email" />
+const Signup = ({ setIsSignup }) => {
+  const [fields, setFields] = useState({
+    email: '',
+    password: '',
+    username: '',
+  });
+
+  const saveFields = e => {
+    setFields({ ...fields, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <SignupStyles>
+      <Form>
+        <fieldset>
+          <h2>Sign up for an account</h2>
+          <div className="input-group">
+            <div className="input-container">
+              <FaEnvelope className="fa" />
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={fields.email}
+                onChange={saveFields}
+              />
+            </div>
+            <div className="input-container">
+              <FaKey className="fa" />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={fields.password}
+                onChange={saveFields}
+                autoComplete="true"
+              />
+            </div>
+            <div className="input-container">
+              <FaUser className="fa" />
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={fields.username}
+                onChange={saveFields}
+              />
+            </div>
           </div>
-          <div className="input-container">
-            <FaKey className="fa" />
-            <input type="password" name="password" placeholder="Password" />
-          </div>
-          <div className="input-container">
-            <FaUser className="fa" />
-            <input type="text" name="username" placeholder="Username" />
-          </div>
-        </div>
-        <button type="submit">Sign Up</button>
-      </fieldset>
-    </Form>
-    <p>
-      Already have an account? <a onClick={() => setIsSignup(false)}>Sign In</a>
-    </p>
-  </SignupStyles>
-);
+          <button type="submit">Sign Up</button>
+        </fieldset>
+      </Form>
+      <p>
+        Already have an account?{' '}
+        <a onClick={() => setIsSignup(false)}>Sign In</a>
+      </p>
+    </SignupStyles>
+  );
+};
 
 Signup.propTypes = {
   setIsSignup: PropTypes.func.isRequired,
