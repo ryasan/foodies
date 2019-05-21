f3e3f475abce5a6682db4f443987305ab094967c
Ryan Santos <ryansantos86@gmail.com>
2019-05-21 12:20:41 -0700

    prod graphql endpoint

--- backend/src/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  port: process.env.PORT || 8000,
-  secret:
-    process.env.NODE_ENV === 'development'
-      ? process.env.MONGO_PASSWORD
-      : process.env.MLAB_PASSWORD,
-};
--- frontend/constants.js
+++ frontend/constants.js
@@ -1,5 +1,5 @@
 export const devEndpoint = 'http://localhost:5000';
-export const prodEndpoint = '';
+export const prodEndpoint = 'https://backend-htvgqjicgq.now.sh';
 
 export const brakePoints = [350, 500, 750, 1000, 1250];
 export const rowHeight = 40;
