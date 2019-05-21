b9dadc93220da0edfd2aebfd15fab911829a4e38
Ryan Santos <ryansantos86@gmail.com>
2019-05-21 15:55:10 -0700

    damn deploy

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -1,6 +1,6 @@
 const { GraphQLServer } = require('graphql-yoga');
-const Mutation = require('./resolvers/mutation');
-const Query = require('./resolvers/query');
+const Mutation = require('./resolvers/Mutation');
+const Query = require('./resolvers/Query');
 require('./db');
 
 const createServer = () => {
