f8d080cd05a50142aa2e61cd84f048862860ac97
Ryan Santos <ryansantos86@gmail.com>
2019-05-21 16:12:59 -0700

    remove dead code

--- frontend/constants.js
+++ frontend/constants.js
@@ -2,5 +2,3 @@ export const devEndpoint = 'http://localhost:5000';
 export const prodEndpoint = 'https://notpinterest-yoga-prod.herokuapp.com';
 
 export const brakePoints = [350, 500, 750, 1000, 1250];
-export const rowHeight = 40;
-export const rowGap = 10;
