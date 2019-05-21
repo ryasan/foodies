39d16b76868847c19d0324db9c6e77f11070639e
Ryan Santos <ryansantos86@gmail.com>
2019-05-21 16:04:17 -0700

    prod endpoint on frontend

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,5 +1,5 @@
 export const devEndpoint = 'http://localhost:5000';
-export const prodEndpoint = '';
+export const prodEndpoint = 'https://notpinterest-yoga-prod.herokuapp.com';
 
 export const brakePoints = [350, 500, 750, 1000, 1250];
 export const rowHeight = 40;
