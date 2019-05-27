dc3fa93bbbe4604a800f89bd66c5401a22b1afcd
Ryan Santos <ryansantos86@gmail.com>
2019-05-27 12:28:54 -0700

    commit

--- frontend/pages/home.js
+++ frontend/pages/home.js
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import { Query } from 'react-apollo';
 
 import ALL_PINS_QUERY from '../graphql/queries/pins';
@@ -6,20 +7,66 @@ import Loader from '../components/Loader/Loader';
 import MasonryHOC from '../components/Masonry/MasonryHOC';
 import { limit } from '../constants';
 
+const HomeStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  .loader {
+    position: absolute;
+    bottom: 1rem;
+  }
+`;
+
 const HomePage = () => {
   return (
     <Query query={ALL_PINS_QUERY} variables={{ skip: 0, limit }}>
-      {({ data: { pins }, error, loading }) => {
+      {({ data: { pins }, error, loading, fetchMore }) => {
         if (error) return <ErrorMessage error={error} />;
         if (loading) {
           return (
-            <div style={{ margin: '5rem' }}>
-              <Loader />
-            </div>
+            <HomeStyles>
+              <MasonryHOC
+                collection={pins}
+                onLoadMore={() => {
+                  fetchMore({
+                    variables: {
+                      skip: pins.length,
+                    },
+                    updateQuery: (prev, { fetchMoreResult }) => {
+                      if (!fetchMoreResult) return prev;
+                      return Object.assign({}, prev, {
+                        pins: [...prev.pins, ...fetchMoreResult.pins],
+                      });
+                    },
+                  });
+                }}
+              />
+              <Loader className="loader" />
+            </HomeStyles>
           );
         }
 
-        return <MasonryHOC pins={pins} onLoadMore={() => ({})} />;
+        return (
+          <HomeStyles>
+            <MasonryHOC
+              pins={pins}
+              onLoadMore={() => {
+                fetchMore({
+                  variables: {
+                    skip: pins.length,
+                  },
+                  updateQuery: (prev, { fetchMoreResult }) => {
+                    if (!fetchMoreResult) return prev;
+                    return Object.assign({}, prev, {
+                      pins: [...prev.pins, ...fetchMoreResult.pins],
+                    });
+                  },
+                });
+              }}
+            />
+          </HomeStyles>
+        );
       }}
     </Query>
   );
